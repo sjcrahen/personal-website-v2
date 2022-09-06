f4921b9498a504074a2b796ba3f8317b2e3cb9f5
shawn j crahen <shawn.j.crahen@gmail.com>
2022-09-05 19:33:04 -0500

    tweaks

--- index.js
+++ index.js
@@ -1,183 +1,190 @@
-// variables
-const modalForm = document.querySelector("#modal-form");
-const form = document.querySelector("#form");
-const inputs = document.querySelectorAll("form > input, form > textarea");
-const sendButtons = document.querySelectorAll(".submit-button");
-const resumeModal = document.querySelector("#resume-modal");
-const backdrop = document.querySelector("#backdrop");
-const downloadIcon = document.querySelector("#download-icon");
-const resumeCloseButton = document.querySelector("#close-resume-modal");
-const contactModal = document.querySelector("#contact-modal");
-const contactCloseButton = document.querySelector("#close-contact-modal");
-const nav = document.querySelector("#nav");
-const navContainer = document.querySelector("#nav-container");
-const navLinks = document.querySelectorAll("#nav-container a");
-const contactLink = document.querySelector("#contact-link");
-const topLinks = document.querySelectorAll("div h2 a");
-const resumeLink = document.querySelector(".about-flex-p a");
-const hamburger = document.querySelector("#hamburger");
+window.onload = new (function () {
+  // variables
+  const modalForm = document.querySelector("#modal-form");
+  const form = document.querySelector("#form");
+  const inputs = document.querySelectorAll("form > input, form > textarea");
+  const sendButtons = document.querySelectorAll(".submit-button");
+  const resumeModal = document.querySelector("#resume-modal");
+  const backdrop = document.querySelector("#backdrop");
+  const downloadIcon = document.querySelector("#download-icon");
+  const resumeCloseButton = document.querySelector("#close-resume-modal");
+  const contactModal = document.querySelector("#contact-modal");
+  const contactCloseButton = document.querySelector("#close-contact-modal");
+  const nav = document.querySelector("#nav");
+  const navContainer = document.querySelector("#nav-container");
+  const navLinks = document.querySelectorAll("#nav-container a");
+  const contactLink = document.querySelector("#contact-link");
+  const topLinks = document.querySelectorAll("div h2 a");
+  const resumeLink = document.querySelector(".about-flex-p a");
+  const hamburger = document.querySelector("#hamburger");
 
-// listeners
-modalForm.addEventListener("submit", sendEmail);
-form.addEventListener("submit", sendEmail);
-inputs.forEach((input) => input.addEventListener("focus", resetSendButton));
-window.addEventListener("resize", initResponsiveNav);
-navLinks.forEach((link) => link.addEventListener("click", showHideMenu));
-contactLink.addEventListener("click", showContact);
-topLinks.forEach((link) => link.addEventListener("click", collapseMenu));
-resumeLink.addEventListener("click", getResume);
-hamburger.addEventListener("click", toggleHamburger);
-
-//handle response nav bar on window size
-function initResponsiveNav() {
-  if (window.innerWidth < 601) {
-    collapseMenu();
-  } else {
-    navContainer.classList.remove("sm");
-  }
-}
-
-// toggle menu for small screens
-function showHideMenu() {
-  if (nav.className !== "main-nav open") {
-    nav.classList.add("open");
-    navLinks.forEach((e) => e.classList.add("responsive"));
-    navContainer.classList.remove("transparent");
-  } else {
-    collapseMenu();
-  }
-}
-
-function collapseMenu() {
-  if (window.innerWidth < 601) {
-    nav.classList.remove("open");
-    navLinks.forEach((e) => e.classList.remove("responsive"));
-    navContainer.classList.add("transparent");
-  } else {
+  if (window.innerWidth > 600) {
     navContainer.classList.remove("transparent");
   }
-  hamburger.classList.remove("open");
-}
 
-function toggleHamburger() {
-  if (hamburger.classList.contains("open")) {
+  // listeners
+  modalForm.addEventListener("submit", sendEmail);
+  form.addEventListener("submit", sendEmail);
+  inputs.forEach((input) => input.addEventListener("focus", resetSendButton));
+  window.addEventListener("resize", initResponsiveNav);
+  navLinks.forEach((link) => link.addEventListener("click", showHideMenu));
+  contactLink.addEventListener("click", showContact);
+  topLinks.forEach((link) => link.addEventListener("click", collapseMenu));
+  resumeLink.addEventListener("click", getResume);
+  hamburger.addEventListener("click", toggleHamburger);
+
+  //handle response nav bar on window size
+  function initResponsiveNav() {
+    if (window.innerWidth < 601) {
+      collapseMenu();
+    } else {
+      navContainer.classList.remove("transparent");
+    }
+  }
+
+  // toggle menu for small screens
+  function showHideMenu() {
+    if (nav.className !== "main-nav open") {
+      nav.classList.add("open");
+      navLinks.forEach((e) => e.classList.add("responsive"));
+      navContainer.classList.remove("transparent");
+    } else {
+      collapseMenu();
+    }
+  }
+
+  function collapseMenu() {
+    if (window.innerWidth < 601) {
+      nav.classList.remove("open");
+      navLinks.forEach((e) => e.classList.remove("responsive"));
+      navContainer.classList.add("transparent");
+    } else {
+      navContainer.classList.remove("transparent");
+    }
     hamburger.classList.remove("open");
-  } else {
-    hamburger.classList.add("open");
+    navContainer.classList.remove("large");
   }
-  showHideMenu();
-}
 
-// send email to aws api gateway -> aws lambda -> aws ses
-function sendEmail(event) {
-  event.preventDefault();
-  const form = event.target;
-  if (form.id === "modal-form") {
-    var button = document.querySelector("#modal-submit");
-  } else {
-    var button = document.querySelector("#submit");
+  function toggleHamburger() {
+    if (hamburger.classList.contains("open")) {
+      hamburger.classList.remove("open");
+    } else {
+      hamburger.classList.add("open");
+    }
+    showHideMenu();
   }
-  if (button) {
-    button.value = "Sending ...";
-  }
-  const {
-    name,
-    email,
-    subject,
-    message,
-    namedc6e35a706d68e5e7908150245876d24a6208f0f,
-    emaildc6e35a706d68e5e7908150245876d24a6208f0f,
-    subjectdc6e35a706d68e5e7908150245876d24a6208f0f,
-    messagedc6e35a706d68e5e7908150245876d24a6208f0f,
-  } = event.target;
-  if (
-    name.value !== "" ||
-    email.value !== "" ||
-    subject.value !== "" ||
-    message.value !== ""
-  ) {
-    return;
-  }
-  const endpoint =
-    "https://53wpstdce3.execute-api.us-east-1.amazonaws.com/default/sendEmail";
-  const body = JSON.stringify({
-    senderName: namedc6e35a706d68e5e7908150245876d24a6208f0f.value,
-    senderEmail: emaildc6e35a706d68e5e7908150245876d24a6208f0f.value,
-    subject: subjectdc6e35a706d68e5e7908150245876d24a6208f0f.value,
-    message: messagedc6e35a706d68e5e7908150245876d24a6208f0f.value,
-  });
-  const requestOptions = {
-    method: "POST",
-    body,
-  };
-  fetch(endpoint, requestOptions)
-    .then((response) => {
-      if (!response.ok) throw new Error("Error in fetch");
-      return response.json();
-    })
-    .then((response) => {
-      if (button) {
-        button.value = "Thank you!";
-        button.style.color = "";
-        button.style.backgroundColor = "";
-        form.reset();
-      }
-    })
-    .catch((error) => {
-      if (button) {
-        button.value = "ERROR!";
-        button.style.color = "red";
-        button.style.backgroundColor = "";
-        form.reset();
-      }
+
+  // send email to aws api gateway -> aws lambda -> aws ses
+  function sendEmail(event) {
+    event.preventDefault();
+    const form = event.target;
+    if (form.id === "modal-form") {
+      var button = document.querySelector("#modal-submit");
+    } else {
+      var button = document.querySelector("#submit");
+    }
+    if (button) {
+      button.value = "Sending ...";
+    }
+    const {
+      name,
+      email,
+      subject,
+      message,
+      namedc6e35a706d68e5e7908150245876d24a6208f0f,
+      emaildc6e35a706d68e5e7908150245876d24a6208f0f,
+      subjectdc6e35a706d68e5e7908150245876d24a6208f0f,
+      messagedc6e35a706d68e5e7908150245876d24a6208f0f,
+    } = event.target;
+    if (
+      name.value !== "" ||
+      email.value !== "" ||
+      subject.value !== "" ||
+      message.value !== ""
+    ) {
+      return;
+    }
+    const endpoint =
+      "https://53wpstdce3.execute-api.us-east-1.amazonaws.com/default/sendEmail";
+    const body = JSON.stringify({
+      senderName: namedc6e35a706d68e5e7908150245876d24a6208f0f.value,
+      senderEmail: emaildc6e35a706d68e5e7908150245876d24a6208f0f.value,
+      subject: subjectdc6e35a706d68e5e7908150245876d24a6208f0f.value,
+      message: messagedc6e35a706d68e5e7908150245876d24a6208f0f.value,
     });
-}
-
-function resetSendButton() {
-  sendButtons.forEach((btn) => {
-    btn.value = "Send";
-    btn.style.color = "";
-  });
-}
-
-// open and close resume modal
-function getResume() {
-  // for larger viewports show modal
-  if (window.innerWidth >= 992) {
-    //show modal
-    resumeModal.style.display = "block";
-    backdrop.style.display = "block";
-
-    // download the resume
-    downloadIcon.onclick = () => openResume();
-
-    // allow closing the modal
-    resumeCloseButton.onclick = () => {
-      resumeModal.style.display = "none";
-      backdrop.style.display = "none";
+    const requestOptions = {
+      method: "POST",
+      body,
     };
-  } else {
-    // small viewports
-    openResume();
+    fetch(endpoint, requestOptions)
+      .then((response) => {
+        if (!response.ok) throw new Error("Error in fetch");
+        return response.json();
+      })
+      .then((response) => {
+        if (button) {
+          button.value = "Thank you!";
+          button.style.color = "";
+          button.style.backgroundColor = "";
+          form.reset();
+        }
+      })
+      .catch((error) => {
+        if (button) {
+          button.value = "ERROR!";
+          button.style.color = "red";
+          button.style.backgroundColor = "";
+          form.reset();
+        }
+      });
   }
-}
 
-function openResume() {
-  window.open("Shawn_Crahen_Resume_2022.pdf");
-}
-
-// show contact modal on large screens
-function showContact() {
-  if (window.innerWidth >= 992) {
-    // show modal
-    contactModal.style.display = "block";
-    backdrop.style.display = "block";
-    // allow closing the modal
-    contactCloseButton.onclick = () => {
-      contactModal.style.display = "none";
-      backdrop.style.display = "none";
-    };
-  } else {
-    location.href = "#contact";
+  function resetSendButton() {
+    sendButtons.forEach((btn) => {
+      btn.value = "Send";
+      btn.style.color = "";
+    });
   }
-}
+
+  // open and close resume modal
+  function getResume() {
+    // for larger viewports show modal
+    if (window.innerWidth >= 992) {
+      //show modal
+      resumeModal.style.display = "block";
+      backdrop.style.display = "block";
+
+      // download the resume
+      downloadIcon.onclick = () => openResume();
+
+      // allow closing the modal
+      resumeCloseButton.onclick = () => {
+        resumeModal.style.display = "none";
+        backdrop.style.display = "none";
+      };
+    } else {
+      // small viewports
+      openResume();
+    }
+  }
+
+  function openResume() {
+    window.open("Shawn_Crahen_Resume_2022.pdf");
+  }
+
+  // show contact modal on large screens
+  function showContact() {
+    if (window.innerWidth >= 992) {
+      // show modal
+      contactModal.style.display = "block";
+      backdrop.style.display = "block";
+      // allow closing the modal
+      contactCloseButton.onclick = () => {
+        contactModal.style.display = "none";
+        backdrop.style.display = "none";
+      };
+    } else {
+      location.href = "#contact";
+    }
+  }
+})();
